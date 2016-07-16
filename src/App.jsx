f015d73223a211bb6fe3cbbdd9842266631a9510
Prabir Shrestha <mail@prabir.me>
2016-07-16 12:27:52 -0700

    added assets and header

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import { observer } from 'mobx-react';
 import DevTools from 'mobx-react-devtools';
+import { Header } from './Header';
 
 @observer
 class App extends Component {
   render() {
     return (
       <div>
-        Hello World
+        <Header />
         <DevTools />
       </div>
     );
---
+++ src/Header.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const Header = () => <header>
+  <div className="container">
+    <h1 className="logo">Hacker News</h1>
+  </div>
+</header>;
